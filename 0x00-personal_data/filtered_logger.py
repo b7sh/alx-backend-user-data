6a05177c89ed42fe7ee27538050690146daf2404
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-07 21:56:21 +0000

    solving the first task ''0. Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+solve - tasks:
+    0. Regex-ing
+    1. Log formatter
+    2. Create logger
+    3. Connect to secure database
+'''
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    '''
+    Arguments:
+        fields: a list of strings
+        representing all fields to obfuscate
+        redaction: a string representing by
+        what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which
+        character is separating all fields in the log line (message)
+        The function should use a regex to replace
+        occurrences of certain field values.
+        filter_datum should be less than 5 lines long
+        and use re.sub to perform the substitution with a single regex.
+    '''
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator,
+                         message)
+        return message
