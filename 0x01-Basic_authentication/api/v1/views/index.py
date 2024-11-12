40ce34a63795fb5b750119072a5831f17b339972
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-12 18:45:27 +0000

    solving the second task '2. Error handler: Forbidden'

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - handle 401 error
     '''
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def Forbidden() -> str:
+    '''
+      GET /api/v1/forbidden
+      - handle 403 error
+    '''
+    abort(403)
