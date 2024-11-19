ff53de5c1e4a0e1750f7ea154e822534cfecde4e
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 06:02:11 +0000

    implementing adding users from the flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,16 +2,32 @@
 '''
 Flask - app
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     ''' the root '''
     return jsonify('{"message": "Bienvenue"}')
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''
+    register for new user - POST
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
