7ff77f40b42bd675aa3bd7daed322c7d24fcd897
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 13:51:05 +0000

    implementing findinf the user using session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,15 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(session_id: str) -> (User | None):
+        '''
+        getting the user using the session id
+        '''
+        if session_id is None:
+            return None
+        try:
+            user = self._db.fund_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
