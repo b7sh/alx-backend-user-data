8ad388f7ef8edd7febf5b86f89b036e230b368f9
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 17:16:13 +0000

    update the reset user password method third time

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,7 +92,7 @@ class Auth:
         '''
         try:
             user = self._db.find_user_by(reset_token)
-            hashed = bcrypt.hashpw(password.encode('utf-8'), gensalt())
+            hashed = _hash_password(password)
             self._db.update_user(user.id,
                                  hashed_password=hashed, reset_token=None)
             return hashed
