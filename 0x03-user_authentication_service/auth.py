f3ef2a4115de1f747c2971e4c69cdea1d874aa56
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 12:47:03 +0000

    implement the check credintials method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
+import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
@@ -36,3 +37,17 @@ class Auth:
             return user
 
         raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''
+        check the user credintials
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(
+                    password.encode('utf-8'),
+                    user.hashed_password):
+                return True
+            return False
+        except NoResultFound:
+            return False
