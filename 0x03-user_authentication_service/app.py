6b8c755f1078ed038b83473e13a45b71e7b71ca0
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 18:08:57 +0000

    updating the reset password function inside the app for the sixth time

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,7 +89,7 @@ def update_password() -> str:
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
     try:
-        user = AUTH.update_password(reset_token, new_password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({"email": f"{email}", "message": "Password updated"}
                        ), 200
     except ValueError:
