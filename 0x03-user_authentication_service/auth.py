27487dd6b8eb69e4b94434315854f419ad389f79
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 17:26:57 +0000

    solve the reset password problem

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,14 +86,17 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
-    def update_password(reset_token: str, password: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         '''
         update the user password
         '''
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed = _hash_password(password)
-            self._db.update_user(user.id,
-                                 hashed_password=hashed, reset_token=None)
+            self._db.update_user(
+                                 user.id,
+                                 hashed_password=hashed,
+                                 reset_token=None
+                                 )
         except NoResultFound:
             raise ValueError
