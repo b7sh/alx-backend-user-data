2b9a9ce11a9e96c3eec86357e7b89cbf9c4b5034
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 17:42:21 +0000

    implementing updating the password in the app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,5 +83,19 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> None:
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        user = AUTH.get_user_by(reset_token)
+        return jsonify(
+                       {"email": f"{email}", "message": "Password updated"}
+                       ), 200
+    except Exception as ex:
+        abort(403, ex)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
