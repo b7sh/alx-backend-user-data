7cdb5d0a6663bff683da4dd021d71ebc341be202
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-19 17:48:09 +0000

    updating the reset password function in the app for the second time

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,7 +84,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> None:
+def update_password() -> str:
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
